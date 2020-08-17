9990c3f991c7067da911ddbeb2e6ba9a0697cc27
Vrund Purohit <vrund14@gmail.com>
2020-08-17 14:44:47 +0530

    Initial Commit

---
+++ app/src/main/java/awesome/vrund/customfontwidgetssample/MainActivity.kt
@@ -0,0 +1,11 @@
+package awesome.vrund.customfontwidgetssample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
